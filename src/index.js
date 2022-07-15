1acf6303d88ade6882416b516396210664e5628d
SheshkoPavel <derzurb@mail.ru>
2022-07-15 13:46:40 +0300

    feat: i solve this task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,50 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' '
 };
 
+
 function decode(expr) {
-    // write your solution here
+    const arr = expr.match(/.{1,10}/g);
+    console.log(arr);
+
+    let arrOfTwo = [];
+    for (let i = 0; i < arr.length; i++) {
+        arrOfTwo.push(arr[i].match(/.{1,2}/g));
+    }
+    console.log(arrOfTwo);
+
+    let arrOfTwoMorze = [];
+    for (let i = 0; i < arrOfTwo.length; i++) {
+        let str = '';
+        for (let j = 0; j < arrOfTwo[i].length; j++) {
+            if (arrOfTwo[i][j] === '10') {
+                str += '.';
+            }
+            if (arrOfTwo[i][j] === '11') {
+                str += '-'; 
+            }
+            if (arrOfTwo[i][j] === '**') {
+                str = ' ';
+                break;
+            }
+        }
+        arrOfTwoMorze.push(str);
+    }
+    console.log(arrOfTwoMorze);
+
+    const morzeToLetters = arrOfTwoMorze.map((e)=> MORSE_TABLE[e]);
+    console.log(morzeToLetters);
+
+    let lettersToString = '';
+    for (let i = 0; i < morzeToLetters.length; i++) {
+        lettersToString += morzeToLetters[i];
+    }
+
+    console.log(lettersToString);
+    return lettersToString;
+
 }
 
 module.exports = {
